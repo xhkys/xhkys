b5989b7084f0f739d2e91b5b9fe57cbe0ec1d85d
xhkys <83496068+xhkys@users.noreply.github.com>
2021-05-22 21:45:46 +0800

    Add files via upload

---
+++ wap.js
@@ -0,0 +1,9 @@
+﻿UA = navigator.userAgent.toLowerCase();
+url = window.location;
+url = url.toString();
+if((UA.indexOf('iphone') != -1 || UA.indexOf('mobile') != -1 || UA.indexOf('android') != -1 || UA.indexOf('ipad') != -1 || UA.indexOf('windows ce') != -1 || UA.indexOf('ipod') != -1) && UA.indexOf('ipod') == -1) {
+url=url.replace("www.","m.");
+Go(url);
+}
+function Go(url) { window.location = url;}
+        
